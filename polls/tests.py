ec5bb47b20e02accf7892a889ca93a84056e4d02
Aseel.alawadg@gmail.com <aseel.alawadh@gmail.com>
2019-09-16 15:17:39 +0300

    + detail view test

--- polls/tests.py
+++ polls/tests.py
@@ -54,3 +54,15 @@ class QuestionIndexViewTest(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.contex['latest_question_list'], ['<Question: past question 2>', '<Question: past question 1>'])
 
+class QuestionDetailViewTest(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='future question', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='past question', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response= self.client.get(url)
+        self.assertContains(response, past_question.question_text)
