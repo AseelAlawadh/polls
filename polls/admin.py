54cce00a9919f0714781d098e1708d87e8e6f3c3
Aseel.alawadg@gmail.com <aseel.alawadh@gmail.com>
2019-09-17 11:12:54 +0300

    +admin file

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question , Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+
+
+class QustionAdmin(admin.ModelAdmin):
+    fields = [(None, {'fields': ['question_text']}),
+                  ('Date information', {'fields': ['pub_date'], 'classes': ['collaps']}),
+             ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QustionAdmin)
+# admin.site.register(Choice)
